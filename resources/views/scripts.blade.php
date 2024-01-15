0c10d615b5f2ce6ba623ad21161672b85cd55ec8
Meherbs <meherbensalah4@gmail.com>
2024-01-15 19:52:16 +0100

    v1.0.2

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -3,3 +3,5 @@
     const laraReportCraftReportsGenerateRoute = "{{ route('lara_report_craft.reports_generate') }}";
     const laraReportCraftReportsPrintingRoute = "{{ route('lara_report_craft.reports_printing') }}";
 </script>
+
+<script src="{{ asset('vendor/lara-report-craft/js/index.js') }}"></script>
