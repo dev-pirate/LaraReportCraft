527e4162615fd6741055187e046c910ff6e02cf1
Meherbs <meherbensalah4@gmail.com>
2024-01-15 19:55:48 +0100

    v1.0.2

--- resources/views/components/reporting_form.blade.php
+++ resources/views/components/reporting_form.blade.php
@@ -1,4 +1,5 @@
-@include('lara_report_craft::scripts')
 <div class="bd-example-snippet bd-code-snippet">
     <div id='lara-report-craft-form'></div>
 </div>
+
+@include('lara_report_craft::scripts')
